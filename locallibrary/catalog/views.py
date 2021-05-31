04821c38f6ffb2e80619612308cf4157d3f224d7
arseniibashkin <84882941+arseniibashkin@users.noreply.github.com>
2021-05-31 15:35:42 +0300

    Initial commit

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+    )
+
+
+from django.views import generic
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+from django.views import generic
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 1
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+
